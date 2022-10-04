431231d2f7d2d1780afe2d084f1a62a869cd542d
wuraolaalli <113648406+wuraolaalli@users.noreply.github.com>
2022-10-03 20:04:41 -0500

    dice

---
+++ Assets/DiceScript/DiceNumberTextScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DiceNumberTextScript : MonoBehaviour {
+    Text text;
+    public static int diceNumber;
+    void Start(){
+        text= GetComponent<Text>();
+    }
+void Update (){
+    text.text= diceNumber.ToString();
+}
+}
